f0d4bad6b36af999dca7ed68431fc3d69cff01b6
audyodi <audytanudjaja@gmail.com>
2017-11-18 13:48:08 +0700

    add helpers to get month and day name

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,9 +3,13 @@
 import populateDotsCoordinate from './populateDotsCoordinate';
 import getPassedDotIndex from './getPassedDotIndex';
 import getAdditionalPassedDotsIndexes from './getAdditionalPassedDotsIndexes';
+import getDayName from './getDayName';
+import getMonthName from './getMonthName';
 
 export {
   populateDotsCoordinate,
   getPassedDotIndex,
-  getAdditionalPassedDotsIndexes
+  getAdditionalPassedDotsIndexes,
+  getDayName,
+  getMonthName
 };
